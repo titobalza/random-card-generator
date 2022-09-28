c419b93456724de7f7630625d519bdc4ed98f3bf
titobalza <titobalzaromero@gmail.com>
2022-09-28 18:09:01 +0000

    ultimo cambio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let rr = document.querySelectorAll(".pinta");
+  let pinta = generapinta();
+  for (var i = 0; i < rr.length; i++) {
+    rr[i].innerHTML = pinta;
+    if (pinta === "&hearts;" || pinta === "&diams;") {
+      rr[i].classList.toggle("red");
+    }
+  }
+  document.querySelector(".numero").innerHTML = generanumero();
+};
+
+let generanumero = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generapinta = () => {
+  let pinta = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+  let indexpinta = Math.floor(Math.random() * pinta.length);
+  return pinta[indexpinta];
 };
